ed7739f839057ad77486286b76e997b002ef3ffa
Silas Almeida <silasadssn@gmail.com>
2020-07-05 22:54:49 -0300

    add: services

---
+++ src/services/api.ts
@@ -0,0 +1,14 @@
+const api = {
+  data: [
+    {
+      id: 'KTOq047D1nk',
+      thumb: 'http://i3.ytimg.com/vi/KTOq047D1nk/maxresdefault.jpg',
+    },
+    {
+      id: 'WVtijTZPZck',
+      thumb: 'http://i3.ytimg.com/vi/WVtijTZPZck/maxresdefault.jpg',
+    },
+  ],
+};
+
+export default api;
